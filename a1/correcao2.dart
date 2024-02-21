b50c83f9aa086f08e00ef76b143cc98ed76fd722
gi rocha <133895231+gymratgi0@users.noreply.github.com>
2024-02-21 11:47:47 -0300

    Add files via upload

---
+++ a1/correcao2.dart
@@ -0,0 +1,10 @@
+void main() {
+  var num = 20;
+
+  //operador ternário
+  var mensagem =
+      num % 2 == 0 ? 'O número $num é par.' : 'O número $num é ímpar.';
+
+  //imprimir a mensagem
+  print(mensagem);
+}
